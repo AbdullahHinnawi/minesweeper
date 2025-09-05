7c851bd29875aeca0c27b708c93bba84f9613564
AbdullahHinnawi <hinnawiabdullah@gmail.com>
2025-09-05 08:28:49 +0300

    Add functions to calculate mine neighbors and reveal empty cells

--- src/pages/Minesweeper/index.tsx
+++ src/pages/Minesweeper/index.tsx
@@ -6,7 +6,7 @@ const ROWS = 8
 const COLS = 8
 const MINES = 10
 
-const Minesweeper = () => {
+const Minesweeper: React.FC = () => {
   const [board, setBoard] = useState<Board>([])
   const [firstClick, setFirstClick] = useState<boolean>(false)
   const [mineCount, setMineCount] = useState<number>(MINES)
@@ -17,6 +17,32 @@ const Minesweeper = () => {
     setFirstClick(true)
     setGameStatus('PLAYING')
   }, [])
+
+  const getNeighborPositions = (row: number, col: number): Array<[number, number]> => {
+    const directions = [-1, 0, 1] // Relative positions: up, same, down
+
+    return directions
+      .flatMap((r) => directions.map((c) => [row + r, col + c] as [number, number]))
+      .filter(([r, c]) => {
+        return (r !== row || c !== col) && r >= 0 && r < ROWS && c >= 0 && c < COLS
+      })
+  }
+
+  // Calculate the count of neighbor mines
+  const calculateMineNeighborCounts = (board: Board) => {
+    const newBoard = board.map((row) => row.map((cell) => ({ ...cell })))
+
+    const nonMineCells: { cell: Cell; rowIndex: number; colIndex: number }[] = newBoard
+      .flatMap((row, rowIndex) => row.map((cell, colIndex) => ({ cell, rowIndex, colIndex })))
+      .filter(({ cell }) => !cell.isMine)
+
+    nonMineCells.forEach(({ cell, rowIndex, colIndex }) => {
+      const neighborPositions: [number, number][] = getNeighborPositions(rowIndex, colIndex)
+      cell.neighborCount = neighborPositions.filter(([r, c]) => newBoard[r][c].isMine).length
+    })
+
+    return newBoard
+  }
 
   const initializeBoard = (): Board => {
     const newBoard = []
@@ -32,7 +58,7 @@ const Minesweeper = () => {
       }
       newBoard.push(currentRow)
     }
-    return newBoard
+    return calculateMineNeighborCounts(newBoard)
   }
 
   const placeMinesRandomly = (board: Board, firstClickRow: number, firstClickCol: number) => {
@@ -53,14 +79,66 @@ const Minesweeper = () => {
     return newBoard
   }
 
+  const revealEmptyCells = (board: Board, row: number, col: number): Board => {
+    const newBoard = board.map(row => row.map(cell => ({ ...cell })));
+    const stack: Array<[number, number]> = [[row, col]];
+
+    while (stack.length > 0) {
+      const lastElement = stack.pop() // Removes the last element from an array and returns it.
+      if(!lastElement) break;
+
+      const [currentRow, currentCol] = lastElement;
+
+      if (currentRow < 0 || currentRow >= ROWS || currentCol < 0 || currentCol >= COLS) {
+        continue;
+      }
+
+      const cell = newBoard[currentRow][currentCol];
+      if (cell.isRevealed || cell.isFlagged || cell.isMine) {
+        continue;
+      }
+
+      cell.isRevealed = true;
+
+      if (cell.neighborCount === 0) {
+        const neighbors = getNeighborPositions(currentRow, currentCol);
+        stack.push(...neighbors);
+      }
+    }
+
+    return newBoard;
+  }
+
   const handleCellClick = (row: number, col: number) => {
+    if (gameStatus !== 'PLAYING') return
+
     const cell = board[row][col]
     let newBoard = board.map((row) => row.map((cell) => ({ ...cell })))
 
     // Handle first click
     if (firstClick) {
       newBoard = placeMinesRandomly(newBoard, row, col)
+      newBoard = calculateMineNeighborCounts(newBoard)
       setFirstClick(false)
+    }
+
+    // If clicked on mine, set game status to lost and reveal all the mines
+    if (newBoard[row][col].isMine) {
+      for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+          if (newBoard[r][c].isMine) {
+            newBoard[r][c].isRevealed = true
+          }
+        }
+      }
+      setGameStatus('LOST')
+    }
+
+    // Reveal cells
+    if (newBoard[row][col].neighborCount === 0) {
+      newBoard = revealEmptyCells(newBoard, row, col);
+    } else {
+      newBoard[row][col].isRevealed = true;
     }
 
     setBoard(newBoard)
@@ -68,6 +146,7 @@ const Minesweeper = () => {
 
   const handleRightClick = (e: React.MouseEvent<HTMLDivElement>, row: number, col: number) => {
     e.preventDefault() // Prevent opening right click menu
+    if (gameStatus !== 'PLAYING') return
 
     const newBoard = board.map((row) => row.map((cell) => ({ ...cell })))
     const cell = newBoard[row][col]
@@ -84,7 +163,7 @@ const Minesweeper = () => {
   const getCellContent = (cell: Cell): string => {
     if (cell.isFlagged) return '🚩'
     if (cell.isMine) return '💣'
-    if (!cell.isRevealed) return ''
+    if (!cell.isRevealed || cell.neighborCount === 0) return ''
     return cell.neighborCount.toString()
   }
 
@@ -129,9 +208,9 @@ const Minesweeper = () => {
                 fontWeight: 'bold',
                 userSelect: 'none',
                 cursor: 'pointer',
-                bgcolor: cell.isMine && cell.isRevealed ? 'red' : cell.isRevealed ? 'grey.200' : '#fff',
+                bgcolor: cell.isMine && cell.isRevealed ? '#bc0000' : cell.isRevealed ? 'grey.200' : '#fff',
                 '&:hover': {
-                  bgcolor: cell.isMine && cell.isRevealed ? 'red' : cell.isRevealed ? 'grey.200' : 'grey.300',
+                  bgcolor: cell.isMine && cell.isRevealed ? '#bc0000' : cell.isRevealed ? 'grey.200' : 'grey.300',
                 },
               }}
               onClick={() => handleCellClick(rowIndex, colIndex)}
